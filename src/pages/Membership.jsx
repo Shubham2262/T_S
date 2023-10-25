4a0f76d85e88982370f8f253de55d96ff4227fe5
Shubham2262 <114879174+Shubham2262@users.noreply.github.com>
2023-10-26 03:11:50 +0530

    first commit

---
+++ src/pages/Membership.jsx
@@ -0,0 +1,80 @@
+import './Membership.css';
+
+
+export default function Membership() {
+    return (
+        <>
+
+            {/* <div class="w-screen min-h-screen bg-gradient p-12 ">
+                <div className='m-10'>
+                    <h1 className='py-10 lg:text-6xl text-xl text-white font-bold text-center'> WHY JOIN US</h1>
+
+                    <div class="gap-5 grid lg:grid-cols-4 grid-cols-2 ">
+
+                        <div class="p-5 rounded-2xl bg-gray-300 hover:bg-green-500 items-center flex flex-col space-y-4 transition-all cursor-pointer relative group">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="w-14 h-14 text-green-500 group-hover:text-white feather feather-award" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" >
+                                <circle cx="12" cy="8" r="7"></circle>
+                                <polyline points="8.21 13.89 7 23 12 20 17 23 15.79 13.88"></polyline>
+                            </svg>
+                            <h2 class="text-gray-800 text-xl group-hover:text-white  font-medium">Recognitions</h2>
+
+                            <p class="text-md text-gray-700 group-hover:text-white text-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque dapibus varius pharetra. Donec vel tempor risus, congue sagittis nisi. Aliquam purus metus, pharetra eget euismod eget, fringilla vel sapien.</p>
+                            
+                        </div>
+
+                        <div class="p-5 rounded-2xl bg-gray-300 hover:bg-green-500 items-center flex flex-col space-y-4 transition-all cursor-pointer relative group">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="w-14 h-14 text-green-500 group-hover:text-white feather feather-award" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" >
+                                <circle cx="12" cy="8" r="7"></circle>
+                                <polyline points="8.21 13.89 7 23 12 20 17 23 15.79 13.88"></polyline>
+                            </svg>
+                            <h2 class="text-gray-800 text-xl group-hover:text-white  font-medium">Recognitions</h2>
+                            
+                            <p class="text-md text-gray-700 group-hover:text-white text-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque dapibus varius pharetra. Donec vel tempor risus, congue sagittis nisi. Aliquam purus metus, pharetra eget euismod eget, fringilla vel sapien.</p>
+                            
+                        </div>
+
+
+                        <div class="p-5 rounded-2xl bg-gray-300 hover:bg-green-500 items-center flex flex-col space-y-4 transition-all cursor-pointer relative group">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="w-14 h-14 text-green-500 group-hover:text-white feather feather-award" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" >
+                                <circle cx="12" cy="8" r="7"></circle>
+                                <polyline points="8.21 13.89 7 23 12 20 17 23 15.79 13.88"></polyline>
+                            </svg>
+                            <h2 class="text-gray-800 text-xl group-hover:text-white  font-medium">Recognitions</h2>
+                            
+                            <p class="text-md text-gray-700 group-hover:text-white text-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque dapibus varius pharetra. Donec vel tempor risus, congue sagittis nisi. Aliquam purus metus, pharetra eget euismod eget, fringilla vel sapien.</p>
+                            
+                        </div>
+
+                        <div class="p-5 rounded-2xl bg-gray-300 hover:bg-green-500 items-center flex flex-col space-y-4 transition-all cursor-pointer relative group">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="w-14 h-14 text-green-500 group-hover:text-white feather feather-award" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" >
+                                <circle cx="12" cy="8" r="7"></circle>
+                                <polyline points="8.21 13.89 7 23 12 20 17 23 15.79 13.88"></polyline>
+                            </svg>
+                            <h2 class="text-gray-800 text-xl group-hover:text-white  font-medium">Recognitions</h2>
+                            
+                            <p class="text-md text-gray-700 group-hover:text-white text-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque dapibus varius pharetra. Donec vel tempor risus, congue sagittis nisi. Aliquam purus metus, pharetra eget euismod eget, fringilla vel sapien.</p>
+                            
+                        </div>
+
+                    </div>
+                </div>
+            </div> */}
+
+
+            <div class="min-h-screen bg-[#09070b] flex flex-col items-center justify-center">
+                <h1 class="text-5xl text-white font-bold mb-8 animate-pulse">
+                    Coming Soon
+                </h1>
+                <p class="text-white text-lg mb-8">
+                    We're working hard to bring you something amazing. Stay tuned!
+                </p>
+            </div>
+
+
+
+
+        </>
+
+
+    )
+}
---
+++ src/pages/Teams.jsx
@@ -0,0 +1,132 @@
+export default function Teams() {
+    return (
+        <>
+
+            {/* <div className="pt-20 overflow-x-hidden">
+
+                <div className="grid text-center justify-center w-full pb-2">
+                    <div className=" bg-emerald-700 w-max rounded-lg  ">
+                        <h2 class=" pt-10 mb-1 text-4xl tracking-tight font-extrabold text-white text-center bg-[#09070b] ">Our Deputy Heads</h2>
+                    </div>
+                </div>
+            </div>
+
+
+            <div className="">
+
+                <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+                   
+                    <div className="grid gap-10 mx-auto sm:grid-cols-2 lg:grid-cols-4 lg:max-w-screen-lg">
+                        <div>
+                            <div className="relative pb-56 mb-4 rounded shadow lg:pb-64">
+                                <img
+                                    className="absolute object-cover w-full h-full rounded"
+                                    src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=3&amp;h=750&amp;w=1260"
+                                    alt="Person"
+                                />
+                            </div>
+                            <div className="flex flex-col sm:text-center">
+                                <p className="text-lg font-bold">Oliver Aguilerra</p>
+                                <p className="mb-5 text-md text-gray-500">Product Manager</p>
+                               
+                            </div>
+                        </div>
+                        <div>
+                            <div className="relative pb-56 mb-4 rounded shadow lg:pb-64">
+                                <img
+                                    className="absolute object-cover w-full h-full rounded"
+                                    src="https://images.pexels.com/photos/2381069/pexels-photo-2381069.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=2&amp;h=750&amp;w=1260"
+                                    alt="Person"
+                                />
+                            </div>
+                            <div className="flex flex-col sm:text-center">
+                                <p className="text-lg font-bold">Marta Clermont</p>
+                                <p className="mb-5 text-md text-gray-500">Design Team Lead</p>
+                              
+                            </div>
+                        </div>
+                        <div>
+                            <div className="relative pb-56 mb-4 rounded shadow lg:pb-64">
+                                <img
+                                    className="absolute object-cover w-full h-full rounded"
+                                    src="https://images.pexels.com/photos/3747435/pexels-photo-3747435.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=2&amp;h=750&amp;w=1260"
+                                    alt="Person"
+                                />
+                            </div>
+                            <div className="flex flex-col sm:text-center">
+                                <p className="text-lg font-bold">Alice Melbourne</p>
+                                <p className="mb-5 text-md text-gray-500">Human Resources</p>
+                              
+                            </div>
+                        </div>
+                        <div>
+                            <div className="relative pb-56 mb-4 rounded shadow lg:pb-64">
+                                <img
+                                    className="absolute object-cover w-full h-full rounded"
+                                    src="https://images.pexels.com/photos/3931603/pexels-photo-3931603.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=2&amp;h=750&amp;w=1260"
+                                    alt="Person"
+                                />
+                            </div>
+                            <div className="flex flex-col sm:text-center">
+                                <p className="text-lg font-bold">John Doe</p>
+                                <p className="mb-5 text-md text-gray-500">Good guy</p>
+                              
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+            <div class="flex items-center justify-center  pt-10 pb-10">
+                <div class="col-span-12">
+                    <div class="overflow-auto lg:overflow-visible ">
+                        <table class="table text-gray-400 border-separate space-y-6 text-sm">
+                            <thead class="bg-gray-800 text-gray-500">
+
+
+                                <tr>
+                                    <th class="p-3"><spam className="font-bold font-serif text-xl text-center "> Name </spam></th>
+                                    <th class="p-3 text-left"><spam className="font-bold font-serif text-xl text-center"> Position</spam></th>
+                                </tr>
+                            </thead>
+
+
+                            <tbody>
+
+
+                                <tr class="bg-gray-900">
+
+                                    <td class="py-5 px-20">
+                                        <spam className="font-bold font-mono text-lg" >Turing</spam>
+                                    </td>
+                                    <td class="py-5 px-20 font-bold">
+                                        <spam className=" font-sans font-bold text-lg">Turing</spam>
+                                    </td>
+                                </tr>
+
+
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div> */}
+
+
+
+            <div class="min-h-screen bg-[#09070b] flex flex-col items-center justify-center">
+                <h1 class="text-5xl text-white font-bold mb-8 animate-pulse">
+                    Coming Soon
+                </h1>
+                <p class="text-white text-lg mb-8">
+                    We're working hard to bring you something amazing. Stay tuned!
+                </p>
+            </div>
+
+
+
+        </>
+
+
+    )
+}
